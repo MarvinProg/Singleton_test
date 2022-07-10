25d75aa14f51a2977a75f1cd7ae61b17efc675da
Artem Lomakin <Lomakin-as@mail.ru>
2022-07-10 15:54:38 +0300

    update: logger to instance

--- app.rb
+++ app.rb
@@ -1,9 +1,32 @@
 class Logger 
+  def initialize 
+    @f = File.open('Log.txt', 'a')
+    # @@f = nil
+  end
+
+  @@f = nil
+
+  def self.instanse_method 
+    if @@f == nil 
+      @@f = Logger.new 
+    end
+
+    return @@f
+  end
+
   def self.say_method
     puts "Hi Artem!"
   end 
+
+  def log_method text
+    @f.puts text
+    # @f.close
+  end
 end
 
-# logger = Logger.new 
-# logger.say_method
 Logger.say_method
+Logger.instanse_method.log_method('Hi Artem 2!')
+Logger.instanse_method.log_method('Hi Artem 3!')
+
+logger = Logger.new 
+logger.log_method('Privet!')
